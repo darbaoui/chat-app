c5f0552188894345c6bfcc6ca36f84ebf61aab20
darbaoui imad <imad@devinweb.com>
2025-06-24 16:22:47 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,10 +63,10 @@ module.exports = {
       colors: {
         chatBox: 'var(--chat-box-color)',
         chat: 'var(--chat-background)',
-        'chat-sp': 'var(--chat-sperator)',
+        'chat-sp': 'var(--chat-seperator)',
         chatBoxMe: {
           DEFAULT: 'var(--chat-box-me)',
-          foreground: 'var(--chat-box-me-forground)',
+          foreground: 'var(--chat-box-me-foreground)',
         },
         sideMenu: 'var(--side_menu-background)',
         title: {
