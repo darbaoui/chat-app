83ae14813dfe7694a95faa79c5ca7d0cb7304f01
darbaoui imad <imad@devinweb.com>
2025-06-25 20:28:27 +0100

    wip

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import MessageVListGroup from "@/components/MessageVListGroup";
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen items-center justify-center  py-8 ">
+    <main className="flex flex-col h-screen items-center justify-center  py-8 global-class">
       <section className="flex-1 max-w-3xl w-full border  rounded-lg overflow-hidden">
         {/* Message list with virtualization and grouping will go here */}
         <MessageVList />
